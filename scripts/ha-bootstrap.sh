52d7b68d31679ab84eb04f02166387d5701132f4
Matt White <matt.white@microsoft.com>
2021-01-15 10:36:08 +0000

    Fix errors setting env vars

--- scripts/ha-bootstrap.sh
+++ scripts/ha-bootstrap.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bashio
 
 # Database things
-export DATABASE_USER=$(bashio::config 'database_user')
-export DATABASE_PASS=$(bashio::config 'database_pass')
-export DATABASE_NAME=$(bashio::config 'database_name')
 export DATABASE_HOST=$(bashio::config 'database_host')
+export DATABASE_NAME=$(bashio::config 'database_name')
+export DATABASE_PASS=$(bashio::config 'database_pass')
 export DATABASE_PORT=$(bashio::config 'database_port')
+export DATABASE_USER=$(bashio::config 'database_user')
 
 # MQTT things
 export DISABLE_MQTT=$(bashio::config 'disable_mqtt')
 export MQTT_HOST=$(bashio::config 'mqtt_host')
-export MQTT_USERNAME=$(bashio::config 'mqtt_user')
+export MQTT_NAMESPACE=$(bashio::config 'mqtt_namespace')
 export MQTT_PASSWORD=$(bashio::config 'mqtt_pass')
-export MQTT_TLS=$(bashio::config 'mqtt_tls')
 export MQTT_TLS_ACCEPT_INVALID_CERTS=$(bashio::config 'mqtt_tls_accept_invalid_certs')
-export MQTT_NAMESPACE=export MQTT_TLS=$(bashio::config 'mqtt_namespace')
+export MQTT_TLS=$(bashio::config 'mqtt_tls')
+export MQTT_USERNAME=$(bashio::config 'mqtt_user')
+
 
 # Other things
-export TZ=$(bashio::config 'timezone')
-export PORT=4000
 export IMPORT_DIR=$(bashio::config 'import_dir')
+export PORT=4000
+export TZ=$(bashio::config 'timezone')
 
 # Import dashboards
 if [ $(bashio::config 'grafana_import_dashboards') == 'true' ]; then
